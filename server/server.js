a160ac080ee526479f3c54494763930e347552b2
krishna <krishna03062003@email.com>
2026-05-19 20:02:54 +0530

    added env setup

--- server/server.js
+++ server/server.js
@@ -3,41 +3,96 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import dns from "dns";
+import axios from "axios";
 
 import router from "./routes/studentRoutes.js";
 
-dns.setDefaultResultOrder("ipv4first");
+dns.setDefaultResultOrder(
+  "ipv4first"
+);
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
 
+// CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://school-management-system-gold-zeta.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+
+// Body Parser
 app.use(express.json());
 
+
+// MongoDB Connect
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    process.env.MONGO_URI
+  )
   .then(() =>
-    console.log("MongoDB Connected")
+    console.log(
+      "MongoDB Connected"
+    )
   )
   .catch((err) =>
     console.log(err)
   );
 
+
+// Routes
 app.use(
   "/api/students",
   router
 );
 
+
+// Home Route
 app.get("/", (req, res) => {
 
-  res.send("API Running");
+  res.send(
+    "API Running"
+  );
 });
 
+
+// Keep Server Alive
+setInterval(async () => {
+
+  try {
+
+    await axios.get(
+      process.env.BASE_URL
+    );
+
+    console.log(
+      "Self Ping Success"
+    );
+
+  } catch (error) {
+
+    console.log(
+      "Ping Failed"
+    );
+  }
+
+}, 300000);
+
+
+
+// PORT
 const PORT =
   process.env.PORT || 5000;
 
+
+// Listen
 app.listen(PORT, () => {
 
   console.log(
